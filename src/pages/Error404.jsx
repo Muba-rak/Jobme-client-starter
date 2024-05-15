3c06d7c3b1e52bbf84f24841ec1d3aec5f0e01ba
Muba-rak <oladapomubarak@gmail.com>
2024-05-15 13:55:48 +0100

    styles: changed error page design

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
+import Err from "../assets/err.jpg";
 
 const Error404 = () => {
   return (
-    <Layout>
-      <section className="page container d-flex justify-content-center align-items-center text-center">
-        <div>
-          <h1 className="text-info display-6">OOOps!</h1>
-          <h3 className="my-3">Page Not Found</h3>
-          <p>This page doesn't exist, we suggest you go back to home</p>
-          <Link
-            to="/"
-            className="btn btn-info text-decoration-none text-white w-75 my-3"
-          >
-            Back Home
-          </Link>
+    <section className="container">
+      <div className="row d-flex justify-content-center align-items-center p-5">
+        <div className="col-lg-6">
+          <img src={Err} alt="" className="img-fluid" />
         </div>
-      </section>
-    </Layout>
+        <div className="col-lg-6 text-center">
+          <div>
+            <h1 className="text-info display-4">OOOps!</h1>
+            <h3 className="my-3">Page Not Found</h3>
+            <p>This page doesn't exist, we suggest you go back to home</p>
+            <Link
+              to="/"
+              className="btn btn-info text-decoration-none text-white w-75 my-3 fs-5"
+            >
+              Back Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
