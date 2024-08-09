40b09dff1248086a0f62beac022f894e2a365f51
Mubarak Oladapo <94548406+Muba-rak@users.noreply.github.com>
2024-08-09 09:36:53 +0100

    Update Empty.jsx

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -4,7 +4,7 @@ const Empty = ({ height, content }) => {
   return (
     <div
       style={{ height }}
-      className="d-flex justify-content-center align-items-center my-3"
+      className="d-flex justify-content-center align-items-center my-4"
     >
       <h2 className="text-dark text-center display-5">{content}</h2>
     </div>
